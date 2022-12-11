83e5fcda1cb5e5d687ab8e37a20899fa1a7939de
Cristian Paulo <cristianpaulo4@gmail.com>
2022-12-11 20:29:52 -0300

    Add project

---
+++ example/lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:date_utils_senuntech/date_utils_senuntech.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<DateTime> listDates = [];
+
+  void _getDates() {
+    setState(() {
+      listDates = DateUtilsSenuntech.mes(
+        dueDate: DateTime(2022, 1, 5),
+        interval: 1,
+        amountOfDate: 10,
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView.builder(
+        itemCount: listDates.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(listDates.elementAt(index).toString()),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _getDates,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ lib/date_utils_senuntech.dart
@@ -0,0 +1,108 @@
+library date_utils_senuntech;
+
+import 'package:flutter/material.dart';
+
+abstract class DateUtilsSenuntech {
+  /// Retorna lista de datas com intervalos em meses, se o dia não estiver no mes atual retornará o ultimo dia do mês
+  /// Exe: 2022-01-31 no mes de fevereiro ficará 2022-02-28 ou se for ano bissexto 2022-02-29.
+  /// Se a data for 31 em meses que não há esse dia também retornará o ultimo dia.
+
+  static List<DateTime> mes({
+    required DateTime dueDate,
+    required int interval,
+    required int amountOfDate,
+  }) {
+    List<DateTime> listDate = [];
+    DateTime dateTime = dueDate;
+
+    for (var i = 0; i < amountOfDate; i++) {
+      dateTime = DateUtils.addMonthsToMonthDate(dueDate, i * interval);
+      int diasCorrenteMes = DateUtils.getDaysInMonth(
+        dateTime.year,
+        dateTime.month,
+      );
+
+      if (dateTime.month == 2 && dueDate.day >= 28) {
+        dateTime = DateTime(
+          dateTime.year,
+          dateTime.month,
+          28,
+        );
+      } else if (dueDate.day > diasCorrenteMes) {
+        dateTime = DateTime(
+          dateTime.year,
+          dateTime.month,
+          diasCorrenteMes,
+        );
+      } else {
+        dateTime = DateTime(
+          dateTime.year,
+          dateTime.month,
+          dueDate.day,
+        );
+      }
+      listDate.add(dateTime);
+    }
+
+    return listDate;
+  }
+
+  /// Retorna lista de datas com intervalos em anos
+  static List<DateTime> ano({
+    required DateTime dueDate,
+    required int interval,
+    required int amountOfDate,
+  }) {
+    List<DateTime> listDate = [];
+    DateTime dateTime = dueDate;
+    listDate.add(dateTime);
+
+    for (var i = 0; i < amountOfDate - 1; i++) {
+      dateTime = DateTime(
+        (dateTime.year + interval),
+        dueDate.month,
+        dueDate.day,
+      );
+
+      listDate.add(dateTime);
+    }
+
+    return listDate;
+  }
+
+  /// Retorna lista de datas com intervalos em semanas
+  static List<DateTime> semana({
+    required DateTime dueDate,
+    required int interval,
+    required int amountOfDate,
+  }) {
+    List<DateTime> listDate = [];
+    DateTime dateTime = dueDate;
+    listDate.add(dateTime);
+
+    for (var i = 0; i < amountOfDate - 1; i++) {
+      dateTime = dateTime.add(Duration(days: (7 * interval)));
+      listDate.add(dateTime);
+    }
+
+    return listDate;
+  }
+
+  /// Retorna lista de datas com intervalos em dias
+  static List<DateTime> dia({
+    required DateTime dueDate,
+    required int interval,
+    required int amountOfDate,
+  }) {
+    List<DateTime> listDate = [];
+    DateTime dateTime = dueDate;
+    listDate.add(dateTime);
+
+    for (var i = 0; i < amountOfDate - 1; i++) {
+      dateTime = dateTime.add(Duration(days: interval));
+      listDate.add(dateTime);
+    }
+
+    return listDate;
+  }
+}
